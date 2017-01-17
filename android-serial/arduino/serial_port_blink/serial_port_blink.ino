1f95eff2a264d24f02e8e2313050f59aabaf29c4
Volodymyr (T-Rex) Triapichko <babloid@gmail.com>
2017-01-17 02:31:09 +0200

    Added android-serial project

---
+++ android-serial/arduino/serial_port_blink/serial_port_blink.ino
@@ -0,0 +1,22 @@
+
+bool state = false;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  while (!Serial) {}
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    for(int i = 0; i < 3; i++) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(50); 
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(50); 
+    }
+    state = (bool) Serial.read();
+  }
+  digitalWrite(LED_BUILTIN, state ? HIGH : LOW);
+  delay(100);  
+}
